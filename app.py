629b614d785cfa235199a14fedb80e9710bc1cf1
Rahul Katiyar <rahulkatiyar19955@gmail.com>
2019-06-18 15:31:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ def func():
 
 @app.route('/admin')
 def admin():
-    return 'You have admin access ' + name
+    return 'You have admin access '
 
-@app.route('/<string:nam>')
+@app.route('/<string:name>')
 def name1(name):
     if(name[0]== 'A' or name[0] == 'a'):
         return redirect(url_for('admin'))
     return 'hello  ' + name
 
-portno = os.environ.get('PORT')
+portno = os.environ.get('PORT',5000)
 app.run(port=portno,host= '0.0.0.0',debug = True)
